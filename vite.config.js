1599915c7f75f6ce67b3db674241052d0d163b34
andrecosmos <andrecosmos.1992@gmail.com>
2026-09-15 13:31:38 -0300

    Versão 2.0.0

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    ...(process.env.VERCEL ? {} : {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5005',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
+    }),
+  },
 })
